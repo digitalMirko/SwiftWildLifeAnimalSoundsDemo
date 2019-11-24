bad812539ad5a1a0f408aced21b952073cad6553
Mirko Cukich <mirkocukich@gmail.com>
2019-11-23 22:48:34 -0600

    IBOutlet and IBAction connected on ViewController_swift from Storyboard

--- Wild Life Sounds/ViewController.swift
+++ Wild Life Sounds/ViewController.swift
@@ -11,12 +11,60 @@ import UIKit
 import AVFoundation
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var apeButtonOutlet: UIButton!
+    @IBOutlet weak var elephantButtonOutlet: UIButton!
+    @IBOutlet weak var hippoButtonOutlet: UIButton!
+    @IBOutlet weak var lionButtonOutlet: UIButton!
+    @IBOutlet weak var tigerButtonOutlet: UIButton!
+    @IBOutlet weak var zebraButtonOutlet: UIButton!
+    
+    @IBOutlet weak var volumeSliderOutlet: UISlider!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
-
-
+    
+    @IBAction func apeButtonClicked(_ sender: Any) {
+        
+        
+    }
+    
+    @IBAction func elephantButtonClicked(_ sender: Any) {
+        
+        
+    }
+    
+    @IBAction func hippoButtonClicked(_ sender: Any) {
+        
+        
+    }
+    
+    @IBAction func lionButtonClicked(_ sender: Any) {
+        
+        
+    }
+    
+    @IBAction func tigerButtonClicked(_ sender: Any) {
+        
+        
+        
+    }
+    
+    @IBAction func zebraButtonClicked(_ sender: Any) {
+        
+        
+    }
+    
+    
+    @IBAction func volumeChanged(_ sender: Any) {
+        
+        
+    }
+    
+    
+    
+    
 }
 
