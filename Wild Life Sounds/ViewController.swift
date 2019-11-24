de4538b196eee06923dc9ec51214c0c640da2056
Mirko Cukich <mirkocukich@gmail.com>
2019-11-23 23:42:05 -0600

    Program Complete

--- Wild Life Sounds/ViewController.swift
+++ Wild Life Sounds/ViewController.swift
@@ -20,6 +20,13 @@ class ViewController: UIViewController {
     @IBOutlet weak var zebraButtonOutlet: UIButton!
     
     @IBOutlet weak var volumeSliderOutlet: UISlider!
+    
+    var soundEffectApe: AVAudioPlayer = AVAudioPlayer()
+    var soundEffectElephant: AVAudioPlayer = AVAudioPlayer()
+    var soundEffectHippo: AVAudioPlayer = AVAudioPlayer()
+    var soundEffectLion: AVAudioPlayer = AVAudioPlayer()
+    var soundEffectTiger: AVAudioPlayer = AVAudioPlayer()
+    var soundEffectZebra: AVAudioPlayer = AVAudioPlayer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,52 +35,70 @@ class ViewController: UIViewController {
         apeButtonOutlet.imageView?.contentMode = UIView.ContentMode.scaleAspectFit
         elephantButtonOutlet.imageView?.contentMode = UIView.ContentMode.scaleAspectFit
         hippoButtonOutlet.imageView?.contentMode = UIView.ContentMode.scaleAspectFit
-        lionButtonOutlet.imageView?.contentMode = UIView.ContentMode.scaleAspectFit
+        lionButtonOutlet.imageView?.contentMode = UIView.ContentMode.scaleAspectFill
         tigerButtonOutlet.imageView?.contentMode = UIView.ContentMode.scaleAspectFit
         zebraButtonOutlet.imageView?.contentMode = UIView.ContentMode.scaleAspectFit
         
+        let soundFileApe = Bundle.main.path(forResource: "ape", ofType: "mp3")
+        let soundFileElephant = Bundle.main.path(forResource: "elephant", ofType: "mp3")
+        let soundFileHippo = Bundle.main.path(forResource: "hippopotamus", ofType: "mp3")
+        let soundFileLion = Bundle.main.path(forResource: "lion", ofType: "mp3")
+        let soundFileTiger = Bundle.main.path(forResource: "tiger", ofType: "mp3")
+        let soundFileZebra = Bundle.main.path(forResource: "zebra", ofType: "mp3")
         
+        do {
+            try soundEffectApe = AVAudioPlayer(contentsOf: URL(fileURLWithPath: soundFileApe!))
+            try soundEffectElephant = AVAudioPlayer(contentsOf: URL(fileURLWithPath: soundFileElephant!))
+            try soundEffectHippo = AVAudioPlayer(contentsOf: URL(fileURLWithPath: soundFileHippo!))
+            try soundEffectLion = AVAudioPlayer(contentsOf: URL(fileURLWithPath: soundFileLion!))
+            try soundEffectTiger = AVAudioPlayer(contentsOf: URL(fileURLWithPath: soundFileTiger!))
+            try soundEffectZebra = AVAudioPlayer(contentsOf: URL(fileURLWithPath: soundFileZebra!))
+        }
+        
+        catch {
+            print(error)
+        }
     }
     
     @IBAction func apeButtonClicked(_ sender: Any) {
         
-        
+        soundEffectApe.play()
     }
     
     @IBAction func elephantButtonClicked(_ sender: Any) {
         
-        
+        soundEffectElephant.play()
     }
     
     @IBAction func hippoButtonClicked(_ sender: Any) {
         
-        
+        soundEffectHippo.play()
     }
     
     @IBAction func lionButtonClicked(_ sender: Any) {
         
-        
+        soundEffectLion.play()
     }
     
     @IBAction func tigerButtonClicked(_ sender: Any) {
         
-        
+        soundEffectTiger.play()
         
     }
     
     @IBAction func zebraButtonClicked(_ sender: Any) {
         
-        
+        soundEffectZebra.play()
     }
     
     
     @IBAction func volumeChanged(_ sender: Any) {
         
-        
+        soundEffectApe.volume = volumeSliderOutlet.value
+        soundEffectElephant.volume = volumeSliderOutlet.value
+        soundEffectHippo.volume = volumeSliderOutlet.value
+        soundEffectLion.volume = volumeSliderOutlet.value
+        soundEffectTiger.volume = volumeSliderOutlet.value
+        soundEffectZebra.volume = volumeSliderOutlet.value
     }
-    
-    
-    
-    
 }
-
